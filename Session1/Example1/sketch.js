39666a498a38fd8022883428477279a1549cf0b3
StavrosDidakis <stavrosdee@msn.com>
2017-11-16 23:09:54 +0000

    Set console message

--- Session1/Example1/sketch.js
+++ Session1/Example1/sketch.js
@@ -24,7 +24,7 @@ function draw() {
   fill(r, g, b, 127);
   ellipse(360, 200, 200, 200);
   //Print on the console the X position of the mouse
-  console.log(mouseX);
+  console.log("mouseX position: " + mouseX);
 }
 
 // When the user clicks the mouse
