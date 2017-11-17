e2af258c121cf17ac87778c52983b434137e88b9
StavrosDidakis <stavrosdee@msn.com>
2017-11-17 14:23:09 +0000

    Added examples

---
+++ Session1/00_Template/sketch.js
@@ -0,0 +1,21 @@
+//https://p5js.org
+//https://p5js.org/learn/
+
+//DAT405 / GAD405
+//00_Template
+//The purpose of this sketch is to provide a basic template
+//for P5.js projects
+
+//Initialization function
+function setup() {
+  //Print a message to the console - to view using Chrome:
+  //View > Developer > Developer Tools > Console
+  console.log("Initialization: OK")
+}
+
+//Rendering function
+function draw() {
+  //Print a message to the console - to view using Chrome:
+  //View > Developer > Developer Tools > Console
+  console.log("Rendering...")
+}
