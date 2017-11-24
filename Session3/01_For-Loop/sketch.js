3b9872a94ec4408f61a01b8d612de5f621b805b6
StavrosDidakis <stavrosdee@msn.com>
2017-11-24 12:44:18 +0000

    update

--- Session3/01_For-Loop/sketch.js
+++ Session3/01_For-Loop/sketch.js
@@ -13,12 +13,15 @@ function setup() {
 }
 
 function draw() {
+  background(255, 0, 0);
   fill(215, 200, 170);
   //For-loop that will create 10 shapes on the window
-  for (let x = 0; x < 10; x++) {
+  for (let x = 0; x < 500; x+=50) {
     //x position is adjusted by the variable x (+1 on each loop)
     //Thus, x goes from 0 to 10. Multiplying by 50 it will create this sequence:
-    //0, 50, 100, 150, 200, 250, 300, 350, 400, 450 
-    rect(size * x, height/2, size, size);
+    //0, 50, 100, 150, 200, 250, 300, 350, 400, 450
+    rect(x, 0, size, size);
+    rect(x, height/2 - size/2, size, size);
+    rect(x, height-size, size, size);
   }
 }
---
+++ Session3/04_Exercise01-Loops_And_Random_Color_solutionX/sketch.js
@@ -0,0 +1,21 @@
+//https://github.com/stavrosdidakis/DAT-GAD-405_2017
+//https://p5js.org
+
+//DAT405 / GAD405
+//04_Exercise01 - Loops and Random Color
+
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(255);
+  fill(255,0,0);
+
+  translate(0, 0);
+  scale(2);
+  rect (0, 0, 100, 100);
+  rect (150, 150, 100, 100);
+  rect (300, 0, 100, 100);
+}
