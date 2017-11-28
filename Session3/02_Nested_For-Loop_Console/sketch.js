7ccf1e15038c7df55b34dd5b37b28598f169fc67
StavrosDidakis <stavrosdee@msn.com>
2017-11-28 18:51:16 +0000

    Updates

--- Session3/02_Nested_For-Loop_Console/sketch.js
+++ Session3/02_Nested_For-Loop_Console/sketch.js
@@ -7,9 +7,12 @@
 function setup() {
   createCanvas(500, 500);
   noLoop();
+  textSize(22);
+  //textAlign(CENTER);
 }
 
 function draw() {
+  background(60);
   //Run the nested loop for x, and y
   for (let y = 0; y < 10; y++) {
     for (let x = 0; x < 10; x++) {
@@ -18,4 +21,8 @@ function draw() {
       console.log("X: " + x + ", Y: " + y)
     }
   }
+  fill(255);
+  text("Open the Developer > Javascript Console", 10, 100);
+  text("to see the nested-loop number values", 10, 130);
+
 }
